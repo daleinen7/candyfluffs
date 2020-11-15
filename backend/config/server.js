32f0fc9926eaaf07fd51383945b7a562aacfe91a
Brittney Galloway <crlnfllr@gmail.com>
2020-11-14 23:25:37 -0600

    git push

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '43e6857d4ad35aa62716039eabc1a88e'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '4caafdab-7e1b-4867-ba15-e85c6b3f65ec'
-};
