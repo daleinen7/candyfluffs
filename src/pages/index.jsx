fd1735036076fbaa6c2e42daf556a1ce377077ce
daleinen7 <daleinen@gmail.com>
2021-02-27 13:53:27 -0600

    home and necahual pages just display shopping grid

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Grid from '../components/Grid';
 
 export default function Home() {
 	return (
 		<Layout>
-			<p>
-				Someone sent a runner to the feeling I was under for the feeling that I lost today.
-			</p>
-		</Layout>
+      <Grid />
+    </Layout>
 	)
 }
--- src/pages/necahual.jsx
+++ src/pages/necahual.jsx
@@ -1,12 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Grid from '../components/Grid';
 
 export default function Necahual() {
   return (
     <Layout>
-      <p>
-          Lost in a dark for everyone, and you everyone knows you just did what you had to. It's a wild time, to be alive. Burning much more than ever before. Burning down the door. It's a wild time. 
-      </p>
+      <Grid />
     </Layout>
   )
 }
