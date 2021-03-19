6eca5f1f7855fb69e1c5ef3c4bfae96bc116626b
daleinen7 <daleinen@gmail.com>
2021-03-19 16:50:15 -0500

    dynamic product type routes

---
+++ src/pages/productType/{datoCmsProduct.productType}.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../../components/Layout";
+import styled from 'styled-components';
+import GridSquare from '../../components/GridSquare';
+import Img from 'gatsby-image';
+
+const StyledDiv = styled.div`
+  .details {
+    display:flex;
+    flex-direction:row;
+    flex-wrap: nowrap;
+  }
+`;
+
+export default function Product({ data }) {
+	return(
+    <Layout>
+      <StyledDiv>
+        {data.allDatoCmsProduct.edges.map(({node}) => (
+          <GridSquare 
+            title={node.title} 
+            price={node.price}
+            image={node.image[0]}
+            slug={node.slug}
+            key={node.id} 
+          />
+        ))}
+        
+      </StyledDiv>
+    </Layout>
+  )
+};
+
+export const query = graphql`
+  query AllProductTypes($productType:String!) {
+  allDatoCmsProduct(filter: {productType: {eq: $productType}}) {
+    edges {
+      node {
+        id
+        title
+        price
+        slug
+        image {
+          fluid(maxWidth: 200) {
+            src
+            ...GatsbyDatoCmsFluid
+          }
+        }
+      }
+    }
+  }
+}
+`;
--- src/pages/products/{datoCmsProduct.slug}.jsx
+++ src/pages/products/{datoCmsProduct.slug}.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
-import Layout from "../../components/Layout"
+import Layout from "../../components/Layout";
 import styled from 'styled-components';
 import Img from 'gatsby-image';
 
