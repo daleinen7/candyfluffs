61f91c5b3e05ece845e799f4e3014a3cd9e24f84
daleinen7 <daleinen@gmail.com>
2021-04-07 15:16:44 -0500

    move slider to index

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
-import Slider from "../Slider"
 import styled from 'styled-components';
 
 export default function Layout(props) {
   return(
     <div className="grid-container">
       <Header heading={props.heading} />
-      <Slider />
         <main>
             {props.children}
         </main>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Slider from "../Slider"
 import Grid from '../components/Grid';
 
 export default function Home() {
 	return (
 		<Layout heading = {"Just a girl who likes to draw manga"}>
+      <Slider />
       <Grid />
     </Layout>
 	)
