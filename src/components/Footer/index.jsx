2aff5529379023981e4faec7904cda6e2dcd0905
daleinen7 <daleinen@gmail.com>
2021-04-08 16:35:08 -0500

    layout footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,6 +3,14 @@ import { Link } from 'gatsby';
 import styled from 'styled-components';
 
 const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    font-size: 2.25rem;
+  }
+
   nav {
     ul {
       display: flex;
