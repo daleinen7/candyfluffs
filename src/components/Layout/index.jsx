514721b65e37106c3d77c5cf4521cb6a74822d83
daleinen7 <daleinen@gmail.com>
2021-04-09 08:53:20 -0500

    add global css vars

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
-import styled from 'styled-components';
 
 export default function Layout(props) {
   return(
