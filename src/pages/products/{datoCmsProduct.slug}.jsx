f3b36c8c0b8d8cf50c91fe0894962088365a76ef
daleinen7 <daleinen@gmail.com>
2021-05-25 11:23:16 -0500

    fix aria hidden reverse mistake

--- src/pages/products/{datoCmsProduct.slug}.jsx
+++ src/pages/products/{datoCmsProduct.slug}.jsx
@@ -125,7 +125,7 @@ export default function Product({ data }) {
           
           <button 
             className={variant==="OG" ? "snipcart-add-item selected":"snipcart-add-item"}
-            aria-hidden={variant=="OG" ? "true":"false"}
+            aria-hidden={variant=="OG" ? "false":"true"}
             data-item-id={data.datoCmsProduct.id}
             data-item-price={data.datoCmsProduct.price}
             data-item-description={data.datoCmsProduct.descriptionNode.childMarkdownRemark.html}
@@ -141,7 +141,7 @@ export default function Product({ data }) {
             data.datoCmsProduct.variation.map((vari, idx)=>{
               return <button 
                 className={variant==idx ? "snipcart-add-item selected":"snipcart-add-item"}
-                aria-hidden={variant==idx ? "true":"false"}
+                aria-hidden={variant==idx ? "false":"true"}
                 data-item-id={vari.id}
                 data-item-price={vari.price}
                 data-item-description={data.datoCmsProduct.descriptionNode.childMarkdownRemark.html + " " + vari.title}
